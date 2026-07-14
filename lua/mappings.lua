f19ad9d7db87907fd965cb29e48123af57a459c0
Om <omj82004@gmail.com>
2026-07-14 19:26:12 -0400

    add diffview, todo-comments, trouble, mason-lspconfig, nvim-dap

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,3 +8,7 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+map("n", "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", { desc = "Trouble diagnostics" })
+map("n", "<leader>gd", "<cmd>DiffviewOpen<cr>", { desc = "Diffview open" })
+map("n", "<leader>gh", "<cmd>DiffviewFileHistory<cr>", { desc = "Diffview file history" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -25,4 +25,56 @@ return {
   -- 		},
   -- 	},
   -- },
+
+  -- git: gitsigns already ships in NvChad core, this adds diff/history view
+  {
+    "sindrets/diffview.nvim",
+    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
+  },
+
+  {
+    "folke/todo-comments.nvim",
+    event = "BufReadPost",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {},
+  },
+
+  {
+    "folke/trouble.nvim",
+    cmd = "Trouble",
+    opts = {},
+  },
+
+  -- bridges mason-installed LSP servers to lspconfig automatically
+  -- same trigger as core nvim-lspconfig so it's ready before a real file opens
+  {
+    "mason-org/mason-lspconfig.nvim",
+    event = "User FilePost",
+    dependencies = { "mason-org/mason.nvim" },
+    opts = {
+      ensure_installed = { "html", "cssls" },
+      automatic_installation = true,
+    },
+  },
+
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "rcarriga/nvim-dap-ui",
+      "nvim-neotest/nvim-nio",
+      "theHamsta/nvim-dap-virtual-text",
+    },
+    keys = {
+      { "<leader>db", function() require("dap").toggle_breakpoint() end, desc = "DAP toggle breakpoint" },
+      { "<leader>dc", function() require("dap").continue() end, desc = "DAP continue" },
+      { "<leader>di", function() require("dap").step_into() end, desc = "DAP step into" },
+      { "<leader>do", function() require("dap").step_over() end, desc = "DAP step over" },
+      { "<leader>dO", function() require("dap").step_out() end, desc = "DAP step out" },
+      { "<leader>dr", function() require("dap").repl.toggle() end, desc = "DAP toggle repl" },
+      { "<leader>du", function() require("dapui").toggle() end, desc = "DAP toggle UI" },
+    },
+    config = function()
+      require "configs.dap"
+    end,
+  },
 }
